2655c5353a14a955d0bdd691eaddb0dd05a0ba07
jamiledu <jloyolag@institutobenitojuarez.edu.ec>
2017-11-27 22:54:17 -0500

    BDD

---
+++ Planificación/BDD/script 27nov2017.sql
@@ -0,0 +1,35 @@
+create database LineaBus
+default character set utf8
+collate utf8_unicode_ci;
+use LineaBus;
+
+create table Linea(
+idLinea int not null auto_increment,
+nombre varchar(255)not null,
+numero varchar(255)not null,
+idTipo int,
+primary key (idLinea),
+constraint R1 foreign key Linea(idTipo)references Tipo(idTipo));
+
+create table Ruta(
+idRuta int not null auto_increment,
+descripcion varchar(255)not null,
+idLinea int,
+primary key (idRuta),
+constraint R2 foreign key Ruta(idLinea)references Linea(idLinea));
+
+create table Tipo(
+idTipo int not null auto_increment,
+descripcion varchar(255)not null,
+primary key (idTipo));
+
+create table Horario(
+idHorario int not null auto_increment,
+hora int not null,
+minuto int not null,
+idLinea int,
+primary key (idHorario),
+constraint R3 foreign key Horario(idLinea)references Linea(idLinea));
+
+
+
